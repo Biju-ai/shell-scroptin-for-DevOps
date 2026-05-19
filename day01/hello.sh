a7c52235f78f7f517e196975c359f5297ce76ae3
Biju Shrestha <bijushrestha768@example.com>
2026-05-19 17:19:43 +0545

    day1 day2

---
+++ day01/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This is a biju by and heheh
+
+echo "hello my fucker"
+
+echo "Learn the importance about the life ans include some  of the importance "
---
+++ day02/jetha-lal-ke-dunya.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this is jethala ke world
+
+<<comment
+ anythind you can 
+multi line comment
+comment
+
+name="bryen"
+echo "Name is $name"
+
+echo "enter your name"
+
+read username
+echo "welcom to ubandu $username,date is $(date)"
+
