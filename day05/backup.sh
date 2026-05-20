fe4bbf0c955b5cd246320b5dcc5d920ada7c8276
Biju Shrestha <bijushrestha768@example.com>
2026-05-20 21:43:04 +0545

    new day05

---
+++ day05/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<<readme
+This is a script for backup with 5 day rotation
+
+Useage:
+./backup.sh <path to your source> <path to backup folder>
+
+readme
+
+function display_usage{
+
+	echo "Usage:./backup.sh <path toyou sorce> <path to backup folder>"
+}
+
